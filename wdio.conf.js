9e9a8fbc5986baa486e5976d09d9b01f9b61d330
EINBOX <57449767+EINBOX@users.noreply.github.com>
2022-02-11 18:58:18 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,7 @@ exports.config = {
     // NPM script (see https://docs.npmjs.com/cli/run-script) then the current working
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
-    specs: ['./test/e2e/*.js'],
+    specs: ['./test/*.js'],
     // Patterns to exclude.
     exclude: [
         // 'path/to/excluded/files'
